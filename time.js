5993d280ad6a389df709c82fdaa3fefdf72650a6
Ellen Fiscina <ellfiscina@gmail.com>
2018-04-03 22:07:39 -0300

    france

--- time.js
+++ time.js
@@ -1,20 +1,32 @@
-d3.csv("monarch.csv", function(data){
+d3.queue()
+	.defer(d3.csv, "england.csv")
+	.defer(d3.csv, "france.csv")
+	.await(analyze);
 
-	data.forEach(function (d){
+function analyze(error, england, france){
+	if(error){console.log(error);}
+	england.forEach(function (d){
 		d.Begin = +d.Begin;
 		d.End = +d.End;
 		d.Duration = d.End - d.Begin;
 	});
-	atualizarPlot(data);
-})
+	france.forEach(function (d){
+		d.Begin = +d.Begin;
+		d.End = +d.End;
+		d.Duration = d.End - d.Begin;
+	});
+	console.log(england[0]);
+	console.log(france[0]);
+	atualizarPlot(england, france);
+}
+var m = {top:20, right:15, bottom:20, left:15};
 
-var mySVG = d3.select("svg").append("g");
+var mySVG = d3.select("svg").append("g").attr("transform", "translate(55,0)");
+var mySVG2 = d3.select("svg").append("g").attr("transform", "translate(55,0)");
 
-function atualizarPlot(dataset){
+function atualizarPlot(england, france){
 	var timeBegin = 800,
 		timeEnd = 2050;
-
-	var m = [20, 15, 15, 120];
 
 	var x = d3.scaleLinear().domain([timeBegin, timeEnd]).range([0,1000]);
 
@@ -23,15 +35,15 @@ function atualizarPlot(dataset){
     var xAxisGroup = mySVG
     	.append("g")
     	.attr("class","xAxis")
-    	.attr("transform","translate("+m[1]+",500)");
+    	.attr("transform","translate(15,500)");
 
     xAxisGroup.call(xAxis);
 
 	var opacity = .5;
 
-	var circle = mySVG
+	var circleE = mySVG
 		.selectAll("circle")
-		.data(dataset)
+		.data(england)
 		.enter()
 		.append("circle")
 		.attr("cx", d=>x(d.Begin))
@@ -39,38 +51,42 @@ function atualizarPlot(dataset){
 		.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
 		.style("fill", "red")
 		.style("fill-opacity", opacity);
+	
+	var tip = d3.tip().attr("class", "d3-tip").html(d => d.Begin + " - " + d.Monarch);
+	circleE.call(tip);
 
-	/*var texto = mySVG
-		.selectAll("text")
-		.data(dataset)
-		.enter()
-		.append("text")
-		.attr("x", d=>x(d.Begin))
-		.attr("y", d=>d.Duration+400)
-		.style("visibility", "hidden");*/
-
-	var tip = d3.tip().attr("class", "d3-tip").html(d => d.Begin);
-	circle.call(tip);
-
-	circle
-		.on("mouseover", function(){
+	circleE
+		.on("mouseover", function(d){
 			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*4);
+			tip.show(d);
 		})
 		.on("mouseout", function(){
 			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3);
-		})
-		.on("click", function(d){
-			if(opacity == .5){
-				opacity = 1;
-				tip.show;
-			}
-			else{
-				opacity = .5;
-				tip.hide;
-			}
-			//opacity = opacity == .5 ? 1 : .5;
-			d3.select(this).style("fill-opacity", opacity);
-
+			tip.hide(d);
 		});
 
+	var circleF = mySVG2
+		.selectAll("circle")
+		.data(france)
+		.enter()
+		.append("circle")
+		.attr("cx", d=>x(d.Begin))
+		.attr("cy", d=>d.Duration+200)
+		.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
+		.style("fill", "blue")
+		.style("fill-opacity", opacity);
+	
+	var tip2 = d3.tip().attr("class", "d3-tip").html(d => d.Begin + " - " + d.Monarch);
+	circleF.call(tip2);
+
+	circleF
+		.on("mouseover", function(d){
+			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*4);
+			tip2.show(d);
+		})
+		.on("mouseout", function(){
+			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3);
+			tip2.hide(d);
+		});
+	
 }
