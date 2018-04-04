33fd22fe185cd4f8b912447a62d6b7803a47c322
ellenfiscina <ellfiscina@gmail.com>
2018-04-04 16:02:29 -0300

    mais dados

--- time.js
+++ time.js
@@ -1,9 +1,10 @@
 d3.queue()
 	.defer(d3.csv, "england.csv")
 	.defer(d3.csv, "france.csv")
+	.defer(d3.csv, "autores.csv")
 	.await(analyze);
 
-function analyze(error, england, france){
+function analyze(error, england, france, autor){
 	if(error){console.log(error);}
 	england.forEach(function (d){
 		d.Begin = +d.Begin;
@@ -15,78 +16,84 @@ function analyze(error, england, france){
 		d.End = +d.End;
 		d.Duration = d.End - d.Begin;
 	});
-	console.log(england[0]);
-	console.log(france[0]);
-	atualizarPlot(england, france);
+	autor.forEach(function (d){
+		d.Begin = +d.Begin;
+		d.End = +d.End;
+		d.Duration = d.End - d.Begin;
+	});
+	atualizarPlot(england, france, autor);
 }
 var m = {top:20, right:15, bottom:20, left:15};
 
-var mySVG = d3.select("svg").append("g").attr("transform", "translate(55,0)");
-var mySVG2 = d3.select("svg").append("g").attr("transform", "translate(55,0)");
+var mySvg = d3.select("svg").attr("transform", "translate(150,150)");
+var England = mySvg.append("g").attr("transform", "translate(10,0)");
+var France = mySvg.append("g").attr("transform", "translate(10,0)");
+var Autor = mySvg.append("g").attr("transform", "translate(10,0)");
 
-function atualizarPlot(england, france){
-	var timeBegin = 800,
+function atualizarPlot(england, france, autor){
+	var timeBegin = 700,
 		timeEnd = 2050;
 
 	var x = d3.scaleLinear().domain([timeBegin, timeEnd]).range([0,1000]);
-
+	var colour = d3.scaleOrdinal().range(d3.schemeCategory10);
     var xAxis = d3.axisBottom(x);
-     
-    var xAxisGroup = mySVG
+    var xAxisGroup = mySvg
     	.append("g")
     	.attr("class","xAxis")
-    	.attr("transform","translate(15,500)");
-
+    	.attr("transform","translate(10,500)");
     xAxisGroup.call(xAxis);
 
 	var opacity = .5;
 
-	var circleE = mySVG
-		.selectAll("circle")
-		.data(england)
-		.enter()
-		.append("circle")
-		.attr("cx", d=>x(d.Begin))
-		.attr("cy", d=>d.Duration+400)
-		.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
-		.style("fill", "red")
-		.style("fill-opacity", opacity);
-	
-	var tip = d3.tip().attr("class", "d3-tip").html(d => d.Begin + " - " + d.Monarch);
-	circleE.call(tip);
 
-	circleE
-		.on("mouseover", function(d){
+	var plota = function(svg, dataset, color, translate){
+		return svg
+			.selectAll("circle")
+			.data(dataset)
+			.enter()
+			.append("circle")
+			.attr("cx", d=>x(d.Begin))
+			.attr("cy", d=>d.Duration+translate)
+			.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
+			.style("fill", color)
+			.style("fill-opacity", opacity);
+	}
+	
+	function acao(circulo){
+		circulo
+			.on("mouseover", function(d){
 			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*4);
 			tip.show(d);
 		})
-		.on("mouseout", function(){
+		.on("mouseout", function(d){
 			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3);
 			tip.hide(d);
 		});
+	}
 
-	var circleF = mySVG2
-		.selectAll("circle")
-		.data(france)
-		.enter()
-		.append("circle")
-		.attr("cx", d=>x(d.Begin))
-		.attr("cy", d=>d.Duration+200)
-		.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
-		.style("fill", "blue")
-		.style("fill-opacity", opacity);
-	
-	var tip2 = d3.tip().attr("class", "d3-tip").html(d => d.Begin + " - " + d.Monarch);
-	circleF.call(tip2);
+	var circleE = plota(England, england, "red", 400);
+	var circleF = plota(France, france, "blue", 200);
+	var circleA = plota(Autor, autor, "green", 0);
 
-	circleF
+	var tip = d3.tip().attr("class", "d3-tip")
+				.html(d => d.Begin + " - " + d.Name);
+
+	circleE.call(tip);
+	circleF.call(tip);
+
+	acao(circleE);
+	acao(circleF);
+
+	circleA
+		.attr("cy", d=>d.Duration*4-100)
+		.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*2)
+		.style("fill", d=>(colour(d.Begin)))
 		.on("mouseover", function(d){
-			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*4);
-			tip2.show(d);
-		})
-		.on("mouseout", function(){
 			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3);
-			tip2.hide(d);
+			tip.show(d);
+		})
+		.on("mouseout", function(d){
+			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*2);
+			tip.hide(d);
 		});
-	
 }
