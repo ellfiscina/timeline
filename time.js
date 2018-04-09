3ef4291a7f9394c4261245ccd8d1c01deb9e497f
ellenfiscina <ellfiscina@gmail.com>
2018-04-09 15:41:11 -0300

    legenda

--- time.js
+++ time.js
@@ -44,8 +44,7 @@ var timeBegin = 1450,
 var x = d3.scaleLinear().domain([timeBegin, timeEnd]).range([0,1000]);
 var colour = d3.scaleOrdinal().range(d3.schemeCategory10);
 
-var tip = d3.tip().attr("class", "d3-tip")
-	.html(d => d.Begin + " - " + d.Name);
+var tip = d3.tip().attr("class", "d3-tip").html(d => d.Begin + " - " + d.Name);
 
 function clicka(){
 	if(flag){
@@ -58,6 +57,54 @@ function clicka(){
 	}
 }
 
+var plota = function(type, svg, dataset, color, y){
+	var plot;
+	if (type == 'c'){			
+		plot = svg
+			.selectAll("circle")
+			.data(dataset)
+			.enter()
+			.append("circle")
+			.attr("cx", d=>x(d.Begin))
+			.attr("cy", d=>d.Duration+y)
+			.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
+			.style("fill", color)
+			.style("fill-opacity", .5);	
+	}
+	else{
+		plot = svg
+			.selectAll("rect")
+			.data(dataset)
+			.enter()
+			.append("rect")
+			.attr("x", d=>x(d.Begin))
+			.attr("y", y)
+			.attr("width", d=>x(d.End)-x(d.Begin))
+			.attr("height", 40)
+			.attr("transform","translate(0,1000),scale(1,-1)")
+			.style("fill", color)
+			.style("opacity", .5)
+			.style("stroke", color);
+	}
+	return plot;
+}
+
+function acao(type, obj){
+	obj
+	.on("mouseover", function(d){
+		type == 'c'
+		? d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*4)
+		: d3.select(this).style("opacity", 1);
+		tip.show(d);
+	})
+	.on("mouseout", function(d){
+		type == 'c'
+		? d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
+		: d3.select(this).style("opacity", .5);
+		tip.hide(d);
+	});
+}
+
 function bubble(){
 	d3.selectAll("rect").remove();
     var xAxis = d3.axisBottom(x);
@@ -67,43 +114,15 @@ function bubble(){
     	.attr("transform","translate(100,650)");
     xAxisGroup.call(xAxis);
 
-	var opacity = .5;
-
-	var plota = function(svg, dataset, color, translate){
-		return svg
-			.selectAll("circle")
-			.data(dataset)
-			.enter()
-			.append("circle")
-			.attr("cx", d=>x(d.Begin))
-			.attr("cy", d=>d.Duration+translate)
-			.attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3)
-			.style("fill", color)
-			.style("fill-opacity", opacity);
-	}
-	
-	function acao(circulo){
-		circulo
-			.on("mouseover", function(d){
-			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*4);
-			tip.show(d);
-		})
-		.on("mouseout", function(d){
-			d3.select(this).attr("r", d=>Math.sqrt(d.Duration*4/Math.PI)*3);
-			tip.hide(d);
-		});
-	}
-
-	var circleE = plota(England, dataE, "red", 400);
-	var circleF = plota(France, dataF, "blue", 200);
-	var circleA = plota(Autor, dataA, d=>(colour(d.Begin)), 50);
-
+	var circleE = plota('c', England, dataE, "red", 400);
+	var circleF = plota('c', France, dataF, "blue", 200);
+	var circleA = plota('c', Autor, dataA, d=>(colour(d.Begin)), 50);
 
 	circleE.call(tip);
 	circleF.call(tip);
 
-	acao(circleE);
-	acao(circleF);
+	acao('c', circleE);
+	acao('c', circleF);
 
 	circleA
 		.attr("cy", d=>d.Duration*4-200)
@@ -120,44 +139,15 @@ function bubble(){
 
 function bar(){
 	d3.selectAll("circle").remove();
-	
-	var plota = function(svg, dataset, color, y){
-		return svg
-			.selectAll("rect")
-			.data(dataset)
-			.enter()
-			.append("rect")
-			.attr("x", d=>x(d.Begin))
-			.attr("y", y)
-			.attr("width", d=>x(d.End)-x(d.Begin))
-			.attr("height", 50)
-			.attr("transform","translate(0,1000),scale(1,-1)")
-			.style("fill", color)
-			.style("opacity", .5)
-			.style("stroke", color);
-	}
 
-	function acao(barra){
-		barra
-			.on("mouseover", function(d){
-			d3.select(this).style("opacity", 1);
-			tip.show(d);
-		})
-		.on("mouseout", function(d){
-			d3.select(this).style("opacity", .5);
-			tip.hide(d);
-		});
-	}
-
-	var barE = plota(England, dataE, "red", 500);
-	var barF = plota(France, dataF, "blue", 550);
-	var barA = plota(Autor, dataA, d=>(colour(d.Begin)), 600);
+	var barE = plota('b', England, dataE, "red", 500);
+	var barF = plota('b', France, dataF, "blue", 550);
+	var barA = plota('b', Autor, dataA, d=>(colour(d.Begin)), 600);
 	
 	barA.attr("y", (d,i)=>(600+i*i));
 	tip.direction('e');
 	
-	acao(barE);
-	acao(barF);
-	acao(barA);
-
+	acao('d', barE);
+	acao('d', barF);
+	acao('d', barA);
 }
